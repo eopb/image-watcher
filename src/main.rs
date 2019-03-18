3f4f4333ef6a9078e8f63ebdbc44c1c0bb8c4950
Ethan Brierley <ethanboxx@gmail.com>
2019-03-18 17:08:26 +0000

    really cool resize stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
+use std::path::Path;
 use walkdir::WalkDir;
 
 fn main() {
-    for entry in WalkDir::new(env::current_exe().unwrap().parent().unwrap()) {
-        let entry = entry.unwrap();
-        println!("{}", entry.path().display());
-        println!(
-            "{:#?}",
-            entry.path().metadata().unwrap().modified().unwrap()
-        );
-    }
+    // for entry in WalkDir::new(env::current_exe().unwrap().parent().unwrap()) {
+    //     let entry = entry.unwrap();
+    //     println!("{}", entry.path().display());
+    //     println!(
+    //         "{:#?}",
+    //         entry.path().metadata().unwrap().modified().unwrap()
+    //     );
+    // }
+    let image_path = Path::new("hand_and_book.JPG");
+    println!("{:#?}", image_path.metadata().unwrap().modified().unwrap(),);
+    let img = image::open(image_path).unwrap();
+    let img = img.resize(60, u32::max_value(), image::FilterType::Gaussian);
+    img.save("native/test.png").unwrap();
 }
