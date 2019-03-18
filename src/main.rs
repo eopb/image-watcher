7c7839c89a90f242e5d26d96358e57b40bde0199
Ethan Brierley <ethanboxx@gmail.com>
2019-03-18 16:15:35 +0000

    interesting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    for entry in WalkDir::new(env::current_exe().unwrap().parent().unwrap()) {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+        println!(
+            "{:#?}",
+            entry.path().metadata().unwrap().modified().unwrap()
+        );
+    }
 }
